59d47a6558550383ef89b3bb5f5401ea689c89a0
Justine Cocchi <jucocchi@microsoft.com>
2016-07-15 13:15:07 -0700

    api call responds correctly for any link to a picture

--- Default.aspx
+++ Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" Async="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
@@ -12,13 +12,13 @@
 
     <div class="row">
         <div class="col-md-6">
-            <h2>Upload Your Photo</h2>
+            <h2>Link Your Photo</h2>
             <p>
-                Please upload a photo of your current facial expression. Must be a JPEG, PNG, GIF, or BMP less than 4GB.
+                Please type the URL of a photo of your current facial expression.
             </p><br>
 
-            <asp:FileUpload ID="EmotionPhoto" runat="server"/><br> 
-            <asp:Button ID="ShowResultsBttn" Text="Show Results" runat="server"/><br>
+            <asp:TextBox ID="EmotionPhoto" runat="server"/><br> 
+            <asp:Button ID="ShowResultsBttn" runat="server" OnClick="ShowResultsBttn_Click" Text="Show Results" CssClass="btn btn-default"/><br>
         </div>
         <div class="col-md-6">
             <h2>Results</h2>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,11 +4,69 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using Microsoft.ProjectOxford.Common;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 
 public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    public void ShowResultsBttn_Click(object sender, EventArgs e)
+    {
+        Console.Write("I was clicked!");
+        String emotionPhoto = EmotionPhoto.Text;
+
+        try
+        {
+            System.Diagnostics.Debug.WriteLine("Trying to send request");
+            MakeRequest(emotionPhoto);
+            System.Diagnostics.Debug.WriteLine("After Request ");
+            Console.Read();
+        }
+        catch (Exception exception)
+        {
+            Console.Write("Detection failed. Please make sure that you have the right subscription key and proper URL to detect.");
+            Console.Write(exception.ToString());
+        }
+    }
+
+
+    static async void MakeRequest(String photo)
+    {
+        var client = new HttpClient();
+        var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+        // Request headers
+        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "6470040a779d442b93118357ccff41ce");
+
+        var uri = "https://api.projectoxford.ai/emotion/v1.0/recognize" + queryString;
+
+        HttpResponseMessage response;
+        
+        // Format Body request
+        String picURL= "\"" + photo + "\"";
+        String format = "\"url\":";
+        String requestBody= String.Format("{0} {1}", format, picURL);
+        requestBody = "{" + requestBody + "}";
+
+        System.Diagnostics.Debug.WriteLine("requestBody: " + requestBody);
+
+        // Request body
+        byte[] byteData = Encoding.UTF8.GetBytes(requestBody);
+
+        using (var content = new ByteArrayContent(byteData))
+        {
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            response = await client.PostAsync(uri, content).ConfigureAwait(false);
+        }
     }
 }
